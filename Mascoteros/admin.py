f40aaf40fc10816351f82cbd81524902e8f1602a
María Belén Monini <trejomartin90@gmail.com>
2023-02-24 20:50:55 -0300

    registro de los models al admin.py

--- Mascoteros/admin.py
+++ Mascoteros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Animal)
+admin.site.register(Establecimiento)
+admin.site.register(Producto)
 
 # Register your models here.
